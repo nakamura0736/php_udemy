07e5344958859cca179a9d1075c679a32807f60f
中村圭輝 <tel07360102@gmail.com>
2020-06-07 00:00:47 +0900

    ファイル読み込みについて

---
+++ requiring.php
@@ -0,0 +1,18 @@
+<?php
+// require 'common/common.php';
+require __DIR__ . '/common/common.php';
+echo $commonVariable;
+echo '<br>';
+commonTest();
+
+echo '<br>';
+
+echo __DIR__;
+
+echo '<br>';
+
+echo __FILE__;
+
+// phpinfo();
+
+?>
