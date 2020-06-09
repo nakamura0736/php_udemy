3cfbd04ab5f832e1935927acd7d4b665b84bd124
中村圭輝 <tel07360102@gmail.com>
2020-06-10 02:48:46 +0900

    validation

--- form/input.php
+++ form/input.php
@@ -11,8 +11,8 @@ function h($str){
 }
 
 echo '<pre>';
-// echo var_dump($_POST);
-echo var_dump($_SESSION);
+echo var_dump($_POST);
+// echo var_dump($_SESSION);
 echo '</pre>';
 echo '<br>';
 //入力フォームの練習
@@ -52,7 +52,7 @@ if(!empty($_POST['btn_submit'])){
     ?>
 
     <form method="POST" action="input.php">
-        名前
+        氏名
         <?php if(isset($_POST['your_name'])): ?>
             <input type="text" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
         <?php else: ?>
@@ -65,7 +65,37 @@ if(!empty($_POST['btn_submit'])){
         <?php else: ?>
             <input type="text" name="email">
         <?php endif; ?>
+        <br>
+        ホームページ
+        <?php if(isset($_POST['url'])): ?>
 
+            <input type="url" name="url" value="<?php echo h($_POST['url']); ?>">
+        <?php else: ?>
+            <input type="url" name="url">
+        <?php endif; ?>
+
+        <br>
+        性別
+        <input type="radio" name="gender" value="0">男性</input>
+        <input type="radio" name="gender" value="1">女性</input>
+        <br>
+        年齢
+        <select name="age">
+            <option value="">洗濯してください</option>
+            <option value="1">〜19歳</option>
+            <option value="2">20歳〜29歳</option>
+            <option value="3">30歳〜39歳</option>
+            <option value="4">40歳〜49歳</option>
+            <option value="5">50歳〜59歳</option>
+            <option value="6">60歳〜69歳</option>
+        </select>
+        <br>
+        お問い合わせ内容
+        <textarea name="contact" value="<?php echo h($_POST['contact']); ?>"></textarea>
+        <br>
+        注意事項のチェック
+        <br>
+        <input type="checkbox" name="caution" value="1">注意事項にチェックする
         <br>
         <br>
         <input type="submit" name="btn_confirm" value="確認する">
