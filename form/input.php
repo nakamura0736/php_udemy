49582d7d9c92844d5c530f35999e894be390570f
中村圭輝 <tel07360102@gmail.com>
2020-06-14 13:40:47 +0900

    bootstrap読み込み、インデント短くした

--- form/input.php
+++ form/input.php
@@ -9,7 +9,7 @@ header('X-FRAME-OPTIONS:DENY');
 
 //セキュリティ対策 XSS(Cross-Site Scripting) サニタイズ
 function h($str){
-    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 echo '<pre>';
@@ -29,159 +29,169 @@ $pageFlag = 0 ; //入力
 $error = validation($_POST);
 
 if(!empty($_POST['btn_confirm']) && empty($error)){
-    $pageFlag = 1 ; //確認
+  $pageFlag = 1 ; //確認
 }
 if(!empty($_POST['btn_submit'])){
-    $pageFlag = 2 ; //完了
+  $pageFlag = 2 ; //完了
 }
 
 ?>
 
-<!DOCTYPE html>
-<meta charset="utf-8">
+<!-- Bootstrapのスターターテンプレート -->
+<!doctype html>
+<html lang="ja">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head></head>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+  <title>Hello, world!</title>
+</head>
 <body>
 
 <!-- 入力画面 -->
 <?php if($pageFlag===0): ?>
-    <?php 
-        if(!isset($_SESSION['csrfToken'])){
-            $csrfToken = bin2hex(random_bytes(32)); 
-            $_SESSION['csrfToken'] = $csrfToken;
-        }
-        $token = $_SESSION['csrfToken'];
-    ?>
+  <?php 
+    if(!isset($_SESSION['csrfToken'])){
+      $csrfToken = bin2hex(random_bytes(32)); 
+      $_SESSION['csrfToken'] = $csrfToken;
+    }
+    $token = $_SESSION['csrfToken'];
+  ?>
 
-    <?php if(!empty($_POST['btn_confirm']) && !empty($error)): ?>
-        <ul>
-            <?php foreach($error as $value): ?>
-                <li><?php echo $value; ?></li>
-            <?php endforeach;?>
-        </ul>
+  <?php if(!empty($_POST['btn_confirm']) && !empty($error)): ?>
+    <ul>
+      <?php foreach($error as $value): ?>
+        <li><?php echo $value; ?></li>
+      <?php endforeach;?>
+    </ul>
+  <?php endif; ?>
+
+  <form method="POST" action="input.php">
+    氏名
+    <?php if(isset($_POST['your_name'])): ?>
+      <input type="text" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+    <?php else: ?>
+      <input type="text" name="your_name">
+    <?php endif; ?>
+    <br>
+    メールアドレス
+    <?php if(isset($_POST['email'])): ?>
+      <input type="email" name="email" value="<?php echo h($_POST['email']); ?>">
+    <?php else: ?>
+      <input type="email" name="email">
+    <?php endif; ?>
+    <br>
+    ホームページ
+    <?php if(isset($_POST['url'])): ?>
+
+      <input type="url" name="url" value="<?php echo h($_POST['url']); ?>">
+    <?php else: ?>
+      <input type="url" name="url">
     <?php endif; ?>
 
-    <form method="POST" action="input.php">
-        氏名
-        <?php if(isset($_POST['your_name'])): ?>
-            <input type="text" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
-        <?php else: ?>
-            <input type="text" name="your_name">
-        <?php endif; ?>
-        <br>
-        メールアドレス
-        <?php if(isset($_POST['email'])): ?>
-            <input type="email" name="email" value="<?php echo h($_POST['email']); ?>">
-        <?php else: ?>
-            <input type="email" name="email">
-        <?php endif; ?>
-        <br>
-        ホームページ
-        <?php if(isset($_POST['url'])): ?>
+    <br>
+    性別
+    <input type="radio" name="gender" value="0">男性</input>
+    <input type="radio" name="gender" value="1">女性</input>
+    <br>
+    年齢
+    <select name="age">
+      <option value="">選択してください</option>
+      <option value="1">〜19歳</option>
+      <option value="2">20歳〜29歳</option>
+      <option value="3">30歳〜39歳</option>
+      <option value="4">40歳〜49歳</option>
+      <option value="5">50歳〜59歳</option>
+      <option value="6">60歳〜69歳</option>
+    </select>
+    <br>
+    お問い合わせ内容
+    <?php if(isset($_POST['contact'])): ?>
 
-            <input type="url" name="url" value="<?php echo h($_POST['url']); ?>">
-        <?php else: ?>
-            <input type="url" name="url">
-        <?php endif; ?>
+      <textarea name="contact"><?php echo h($_POST['contact']); ?></textarea>
+    <?php else: ?>
+      <textarea name="contact"></textarea>
+    <?php endif; ?>
 
-        <br>
-        性別
-        <input type="radio" name="gender" value="0">男性</input>
-        <input type="radio" name="gender" value="1">女性</input>
-        <br>
-        年齢
-        <select name="age">
-            <option value="">選択してください</option>
-            <option value="1">〜19歳</option>
-            <option value="2">20歳〜29歳</option>
-            <option value="3">30歳〜39歳</option>
-            <option value="4">40歳〜49歳</option>
-            <option value="5">50歳〜59歳</option>
-            <option value="6">60歳〜69歳</option>
-        </select>
-        <br>
-        お問い合わせ内容
-        <?php if(isset($_POST['contact'])): ?>
-
-            <textarea name="contact"><?php echo h($_POST['contact']); ?></textarea>
-        <?php else: ?>
-            <textarea name="contact"></textarea>
-        <?php endif; ?>
-
-        <br>
-        注意事項のチェック
-        <br>
-        <input type="checkbox" name="caution" value="1">注意事項にチェックする
-        <br>
-        <br>
-        <input type="submit" name="btn_confirm" value="確認する">
-        <input type="hidden" name="csrf" value="<?php echo $token ?>" >
-    </form>
+    <br>
+    注意事項のチェック
+    <br>
+    <input type="checkbox" name="caution" value="1">注意事項にチェックする
+    <br>
+    <br>
+    <input type="submit" name="btn_confirm" value="確認する">
+    <input type="hidden" name="csrf" value="<?php echo $token ?>" >
+  </form>
 <?php endif; ?>
 
 <!-- 確認画面 -->
 <?php if($pageFlag===1): ?>
-    <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
-        <form method="POST" action="input.php">
-            名前
-            <br>
-            <?php echo h($_POST['your_name']); ?>
-            <br>
-            メールアドレス
-            <br>
-            <?php echo h($_POST['email']); ?>
-            <br>
-            ホームページ
-            <?php echo h($_POST['url']); ?>
-            <br>
-            性別
-            <?php 
-                if($_POST['gender'] === '0') echo '男性';
-                if($_POST['gender'] === '1') echo '女性';
-            
-            ?>
-            <br>
-            年齢
-            <?php 
-                if($_POST['age'] === '1') echo '〜19歳';
-                elseif($_POST['age'] === '2') echo '20歳〜29歳';
-                elseif($_POST['age'] === '3') echo '30歳〜39歳';
-                elseif($_POST['age'] === '4') echo '40歳〜49歳';
-                elseif($_POST['age'] === '5') echo '50歳〜59歳';
-                elseif($_POST['age'] === '6') echo '60歳〜69歳';
-            ?>
-            <br>
-            お問い合わせ内容
-            <?php echo h($_POST['contact']); ?>
+  <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+    <form method="POST" action="input.php">
+      名前
+      <br>
+      <?php echo h($_POST['your_name']); ?>
+      <br>
+      メールアドレス
+      <br>
+      <?php echo h($_POST['email']); ?>
+      <br>
+      ホームページ
+      <?php echo h($_POST['url']); ?>
+      <br>
+      性別
+      <?php 
+        if($_POST['gender'] === '0') echo '男性';
+        if($_POST['gender'] === '1') echo '女性';
+      
+      ?>
+      <br>
+      年齢
+      <?php 
+        if($_POST['age'] === '1') echo '〜19歳';
+        elseif($_POST['age'] === '2') echo '20歳〜29歳';
+        elseif($_POST['age'] === '3') echo '30歳〜39歳';
+        elseif($_POST['age'] === '4') echo '40歳〜49歳';
+        elseif($_POST['age'] === '5') echo '50歳〜59歳';
+        elseif($_POST['age'] === '6') echo '60歳〜69歳';
+      ?>
+      <br>
+      お問い合わせ内容
+      <?php echo h($_POST['contact']); ?>
 
-            <br>
-            <br>
-            <input type="submit" name="back" value="戻る">
-            <input type="submit" name="btn_submit" value="送信する">
-            <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
-            <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
-            <input type="hidden" name="url" value="<?php echo h($_POST['url']); ?>">
-            <input type="hidden" name="gender" value="<?php echo h($_POST['gender']); ?>">
-            <input type="hidden" name="age" value="<?php echo h($_POST['age']); ?>">
-            <input type="hidden" name="contact" value="<?php echo h($_POST['contact']); ?>">
+      <br>
+      <br>
+      <input type="submit" name="back" value="戻る">
+      <input type="submit" name="btn_submit" value="送信する">
+      <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+      <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+      <input type="hidden" name="url" value="<?php echo h($_POST['url']); ?>">
+      <input type="hidden" name="gender" value="<?php echo h($_POST['gender']); ?>">
+      <input type="hidden" name="age" value="<?php echo h($_POST['age']); ?>">
+      <input type="hidden" name="contact" value="<?php echo h($_POST['contact']); ?>">
 
-
-
-            <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
-        </form>
-    <?php endif; ?>
+      <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
+    </form>
+  <?php endif; ?>
 <?php endif; ?>
 
 <!-- 完了画面 -->
 <?php if($pageFlag===2): ?>
-    <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
-        送信が完了しました。
-        <?php unset($_SESSION['csrfToken']); ?>
-    <?php endif; ?>
+  <?php if($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+    送信が完了しました。
+    <?php unset($_SESSION['csrfToken']); ?>
+  <?php endif; ?>
 <?php endif; ?>
 
 
-
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
-
+</html>
