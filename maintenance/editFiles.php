382c22ce46b3bf068f84029557c4fcedb83b7f35
中村圭輝 <tel07360102@gmail.com>
2020-06-16 17:27:27 +0900

    ファイル操作１

---
+++ maintenance/editFiles.php
@@ -0,0 +1,17 @@
+<?php
+
+$coontactFile = '.contact.dat';
+
+//ファイル丸ごと読み込み
+$fileContents = file_get_contents($coontactFile);
+echo $fileContents;
+
+//ファイルに書き込み(上書き)
+// file_put_contents($coontactFile, 'テスト');
+
+$addText = 'テストです' . "\n";
+
+//ファイルに書き込み(追記)
+file_put_contents($coontactFile, $addText, FILE_APPEND);
+
+?>
--- maintenance/test.php
+++ maintenance/test.php
@@ -7,4 +7,9 @@ echo __FILE__;
 echo '<br>';
 //パスワード(暗号化)
 echo(password_hash('password123', PASSWORD_BCRYPT));
+
+echo '<br>';
+
+
+
 ?>
